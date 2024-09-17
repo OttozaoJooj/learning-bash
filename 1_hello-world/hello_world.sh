03bc486d1f8fa4b98b8a7de7330b0ac56f8813f1
Otto Mozale <otto.mozale007@gmail.com>
2024-09-17 15:43:42 -0300

    first commit

---
+++ 1_hello-world/hello_world.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#if [ "$Idade" -ge 18 ]; then
+#   echo "Você é maior de Idade"
+#else
+#    echo "Você não é maior de idade"
+#fi
+
+Idade=19  
+nome="lacração no mundo dos games"  
+
+echo "$nome e $Idade"
+
+
+
+
---
+++ 2_variables/variables.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Variáveis String
+nome="Otto"
+
+echo "Olá, meu nome é $nome"
+
+string="$nome é um nome legal"
+
+echo "$string, não acha?"
+
+#Variáveis Numéricas (Inteiras)
+
+idade=20
+mais_dez_anos=$((idade + 10))
+
+
+echo "E eu tenho $idade anos"
+
+echo "daqui a dez anos, terei $mais_dez_anos anos"
+
+#Variáveis Numéricas (Reais)
+
+resultado=$(echo "scale=5; 10/3" | bc)
+
+echo "$resultado é um número ponto flutuante"
+
+
+
+
+
+
+
+
+
+
+
